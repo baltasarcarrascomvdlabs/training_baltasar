0948df743b6e0e8ce11ba62fe0d07487819612ad
baltasarcarrasco <bcarrasco@montevideolabs.com>
2022-05-31 12:16:09 -0300

    Corrected comments and added new code

--- access_dynamo_from_python.py
+++ access_dynamo_from_python.py
@@ -1,30 +1,68 @@
-import json
 import boto3
 from boto3.dynamodb.conditions import Key
+from curses import is_term_resized
+import json
+import mysql.connector
+
+'''
+Function for retrieving an attribute from a RDS database that is used as
+foreign key in another DynamoDb database, and then retrieving the item 
+from the DynamoDb database.
+
+This could be done using the event lambda_handler receives, i did it this
+instead in order to show you the keys and parameters used.
+'''
 
 def lambda_handler(event, context):
-        # TODO implementimport boto3
-    
-    TABLE_NAME = "Personas"
-    
-    # Creating the DynamoDB Client
-    dynamodb_client = boto3.client('dynamodb', region_name="sa-east-1")
-    
-    # Creating the DynamoDB Table Resource
-    dynamodb = boto3.resource('dynamodb', region_name="sa-east-1")
-    table = dynamodb.Table(TABLE_NAME)
-    response = table.get_item(
+    event_for_rds = create_event_for_rds()
+    id = retrieve_foreign_key_from_rds_for_dynamo(event_for_rds)
+    event_for_dynamo = create_event_for_dynamo()
+    item = retrieve_item_from_dynamo_table(event_for_dynamo)
+    return item
+
+def retrieve_item_from_dynamo_table(event):
+    region_name = event["region_name"]
+    dynamodb = boto3.resource('dynamodb', region_name=region_name)
+    table_name = event["table_name"]
+    table = dynamodb.Table(table_name)
+    item_id = event["id"]
+    item = table.get_item(
         Key={
-            'Id': '0',
-            'Edad': '22'
+            'Id': item_id,
         }
     )
-    return {
-        'response' : response,
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
-    
-    #THE CODE FOR ACCESING DE RDS DATABASE I DID IT IN JAVA, AND I HAVE ALREADY MADE QUERIES 
-    # TO RDS DB IN PYTHON, IF YOU WANT TO, I CAN ASS THAT PART TO THIS SCRIPT
-    # ALSO THE KEY IS HARDCODED AS I WAS TRYING TO UNDERSTAND THE FORMAT
+    return item
+
+def create_event_for_dynamo():
+    event = dict()
+    event["table_name"] = 'mvd_labs'
+    event["id"] = '1'
+    event["region_name"] = 'sa-east-1'
+    return event
+
+def retrieve_foreign_key_from_rds_for_dynamo(event):
+    host = event["host"]
+    database = event["database"]
+    user = event["user"]
+    connection = mysql.connector.connect(host=host, database=database, user=user)
+    if connection.is_connected():
+        print("Type a name")
+        name = input() 
+        print("Type a surname")
+        surname = input()
+        cursor = connection.cursor()
+        query = create_query(name, surname, database) 
+        cursor.execute(query)
+        row = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    return row
+
+def create_query(name, surname, database):
+    return "select Id from " + database + "where name = '" + name + "' and surname ='" + surname + "'"
+
+def create_event_for_rds():
+    event = dict()
+    event["host"] = 'database-2.ca4xvkqm7ol9.sa-east-1.rds.amazonaws.com'
+    event["database"] = 'employees'
+    event["user"] = 'root'
