ef7879f49dfd9cb6707dbeea4681841d1f119b1c
baltasarcarrasco <bcarrasco@montevideolabs.com>
2022-05-31 12:55:49 -0300

    Added exceptions and corrected event usage

--- access_dynamo_from_python.py
+++ access_dynamo_from_python.py
@@ -9,60 +9,74 @@ Function for retrieving an attribute from a RDS database that is used as
 foreign key in another DynamoDb database, and then retrieving the item 
 from the DynamoDb database.
 
-This could be done using the event lambda_handler receives, I did it this
-way instead, in order to show you the keys and parameters used.
+Usage:
+Event = {
+    "table_name" : "mvd_labs"
+    "id" : "1"
+    "region_name" : "sa-east-1"
+    "host" : "database-2.ca4xvkqm7ol9.sa-east-1.rds.amazonaws.com"
+    "database" : "employees"
+    "user" : "root"
+}
 '''
 
 def lambda_handler(event, context):
-    event_for_rds = create_event_for_rds()
-    id = retrieve_foreign_key_from_rds_for_dynamo(event_for_rds)
-    event_for_dynamo = create_event_for_dynamo()
-    item = retrieve_item_from_dynamo_table(event_for_dynamo)
-    return item
+    try:
+        id = retrieve_foreign_key_from_rds_for_dynamo(event["host"], event["database"], event["user"])
+        item = retrieve_item_from_dynamo_table(event["region_name"], event["table_name"], event["id"])
+        return item
+    except:
+        raise Exception("Error, something wrong occurred...")
 
-def retrieve_item_from_dynamo_table(event):
-    region_name = event["region_name"]
-    dynamodb = boto3.resource('dynamodb', region_name=region_name)
-    table_name = event["table_name"]
-    table = dynamodb.Table(table_name)
-    item_id = event["id"]
-    item = table.get_item(
-        Key={
-            'Id': item_id,
-        }
-    )
-    return item
+'''
+Function for retrieving an item from a DynamoDb database, receiving a region name, table name, and an item id.
+The function returns the item. 
+'''
 
-def create_event_for_dynamo():
-    event = dict()
-    event["table_name"] = 'mvd_labs'
-    event["id"] = '1'
-    event["region_name"] = 'sa-east-1'
-    return event
+def retrieve_item_from_dynamo_table(region_name, table_name, item_id):
+    try:
+        dynamodb = boto3.resource('dynamodb', region_name=region_name)
+        table = dynamodb.Table(table_name)
+        item = table.get_item(
+            Key={
+                'Id': item_id,
+            }
+        )
+        return item
+    except:
+        raise Exception("Couldn't retrieve RDS database item")
 
-def retrieve_foreign_key_from_rds_for_dynamo(event):
-    host = event["host"]
-    database = event["database"]
-    user = event["user"]
-    connection = mysql.connector.connect(host=host, database=database, user=user)
-    if connection.is_connected():
-        print("Type a name")
-        name = input() 
-        print("Type a surname")
-        surname = input()
-        cursor = connection.cursor()
-        query = create_query(name, surname, database) 
-        cursor.execute(query)
-        row = cursor.fetchone()
-    cursor.close()
-    connection.close()
-    return row
+'''
+Function for retrieving an item Id from a RDS database. It receives a host, a database name, and a user 
+in order to connect to the database. A query is made receiving a Name and Surname as an input. 
+The function returns the id. 
+'''
+
+def retrieve_foreign_key_from_rds_for_dynamo(host, database, user):
+    try:
+        connection = mysql.connector.connect(host=host, database=database, user=user)
+        if connection.is_connected():
+            print("Type a name")
+            name = input() 
+            print("Type a surname")
+            surname = input()
+            cursor = connection.cursor()
+            query = create_query(name, surname, database) 
+            cursor.execute(query)
+            row = cursor.fetchone()
+        else: 
+            raise Exception("Connection to DynamoDb failed")
+        cursor.close()
+        connection.close()
+        return row
+    except:
+        raise Exception("Couldn't retrieve DynamoDb database item")
+
+
+'''
+Function for creating the query retrieving the items with a certain Name and Surname. It also need the
+database name. It returns the query in a string.
+'''
 
 def create_query(name, surname, database):
     return "select Id from " + database + "where name = '" + name + "' and surname ='" + surname + "'"
-
-def create_event_for_rds():
-    event = dict()
-    event["host"] = 'database-2.ca4xvkqm7ol9.sa-east-1.rds.amazonaws.com'
-    event["database"] = 'employees'
-    event["user"] = 'root'
